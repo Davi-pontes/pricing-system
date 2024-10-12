4f1f20b8ae4b80c0e5a34eba0154832a3e941706
Davi Jose <85004283+Davi-pontes@users.noreply.github.com>
2024-10-12 19:43:55 -0300

    style: Modify logica for return values duplicate

--- src/controller/productIngredien/get-all-productIngredient.ts
+++ src/controller/productIngredien/get-all-productIngredient.ts
@@ -5,18 +5,27 @@ import { IProductIngredient } from "@/interfaces/productIngredients";
 import { MySqlGetAllProductIngredientRepository } from "@/repository/productIngredient/get-all-ingredient";
 
 export class GetAllProductIngredientController implements IController {
-    constructor(private readonly mySqlGetAllProductIngredientRepository: MySqlGetAllProductIngredientRepository) { }
+  constructor(
+    private readonly mySqlGetAllProductIngredientRepository: MySqlGetAllProductIngredientRepository
+  ) {}
 
-    async handle(httpRequest: HttpRequest<any>): Promise<HttpResponse<IProductIngredient[] | string>> {
-        try {
-            const idUser = httpRequest.params.idUser
-            
-            const productsIngredients = await this.mySqlGetAllProductIngredientRepository.getAllProductIngredient(idUser)
+  async handle(
+    httpRequest: HttpRequest<any>
+  ): Promise<HttpResponse<IProductIngredient[] | string>> {
+    try {
+      const idUser = httpRequest.params.idUser;
 
-            return ok<IProductIngredient[]>(productsIngredients)
+      const productsIngredients =
+        await this.mySqlGetAllProductIngredientRepository.getAllProductIngredient(
+          idUser
+        );
+        const newArray = productsIngredients.filter((value, index, self) =>
+            index === self.findIndex((v) => v.name === value.name)
+        );
 
-        } catch (error) {
-            return serverError()
-        }
+      return ok<IProductIngredient[]>(newArray);
+    } catch (error) {
+      return serverError();
     }
+  }
 }
--- src/repository/productIngredient/get-all-ingredient.ts
+++ src/repository/productIngredient/get-all-ingredient.ts
@@ -4,7 +4,7 @@ import { IGetAllProductIngredient, IProductIngredient } from "@/interfaces/produ
 export class MySqlGetAllProductIngredientRepository implements IGetAllProductIngredient {
     async getAllProductIngredient(idUser: string): Promise<IProductIngredient[]> {
         const allProduct = await connection
-            .select('pi.id','pi.name','pi.weight','pi.unit1','pi.price','pi.quantity','pi.unit2','pi.ingredient_cost','pi.id_product','pi.quantity_in_stock','pi.total_cash_in_stock')
+            .select('pi.id','pi.name','pi.weight','pi.unit1','pi.price','pi.quantity','pi.unit2','pi.ingredient_cost','pi.quantity_in_stock','pi.total_cash_in_stock')
             .from('category as c')
             .rightJoin('product as p', 'c.id', 'p.id_category')
             .rightJoin('product_ingredients as pi', 'p.id_product', 'pi.id_product')

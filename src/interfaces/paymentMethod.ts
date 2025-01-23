13caf7b6bebe149384c7ea4433bf0a5a4f3ede62
Davi Jose <85004283+Davi-pontes@users.noreply.github.com>
2025-01-22 21:29:13 -0300

    feat: altered payment method

--- src/interfaces/paymentMethod.ts
+++ src/interfaces/paymentMethod.ts
@@ -1,38 +1,25 @@
 export interface IPaymentMethod{
     id: number
     id_user: string
-    type: ETypePayment
-    tax: number
-    status: number
-}
-export interface IPaymentMethodFE{
-    id: number
-    id_user: string
-    type: ETypePaymentFE
+    type: TypePayment
     tax: number
     status: number
 }
 export interface ICreatePaymentMethod{
     id_user: string
-    type: ETypePayment
+    type: TypePayment
     tax: number
     status: number
 }
 
-export enum ETypePayment {
+export enum TypePayment {
     money = 'money',
     credit_card = 'credit_card',
     debit_card = 'debit_card',
     pix = 'pix',
     ticket = 'ticket'
 }
-export enum ETypePaymentFE {
-    dinheiro = 'Dinheiro',
-    cartao_credito = 'Cartão de crédito',
-    cartao_debito = 'Cartão de débito',
-    pix = 'Pix',
-    boleto = 'Boleto'
-}
+
 
 export interface ICreatePaymentMethodRepository{
     createPaymentMehod(params: ICreatePaymentMethod[]): Promise<IPaymentMethod>
@@ -45,5 +32,5 @@ export interface IGetPaymentMethodRepository{
     getByActiveUserId(idUser: string): Promise<IPaymentMethod[]>
 }
 export interface IGetPaymentMethodService{
-    getByActiveUserId(idUser: string): Promise<IPaymentMethodFE[]>
+    getByActiveUserId(idUser: string): Promise<IPaymentMethod[]>
 }
--- src/service/paymentMethod/get-paymentMethod.ts
+++ src/service/paymentMethod/get-paymentMethod.ts
@@ -1,26 +1,11 @@
-import { ETypePayment, ETypePaymentFE, IGetPaymentMethodRepository, IGetPaymentMethodService, IPaymentMethod, IPaymentMethodFE } from "@/interfaces/paymentMethod";
+import { IGetPaymentMethodRepository, IGetPaymentMethodService, IPaymentMethod } from "@/interfaces/paymentMethod";
 
 export class GetPaymentMethodService implements IGetPaymentMethodService{
     constructor (private readonly getPaymentMethodRepository: IGetPaymentMethodRepository){}
     
-    async getByActiveUserId(idUser: string): Promise<IPaymentMethodFE[]> {
+    async getByActiveUserId(idUser: string): Promise<IPaymentMethod[]> {
         const allPaymentMethodActive = await this.getPaymentMethodRepository.getByActiveUserId(idUser)
-        
-        const translationDone = allPaymentMethodActive.map((paymentMethod) => ({
-            ...paymentMethod,
-            type: this.translateToFE(paymentMethod.type),
-        }));
-        
-        return translationDone
-    }
-    private translateToFE(type: ETypePayment): ETypePaymentFE {
-        const typePaymentMap: Record<ETypePayment, ETypePaymentFE> = {
-            [ETypePayment.money]: ETypePaymentFE.dinheiro,
-            [ETypePayment.credit_card]: ETypePaymentFE.cartao_credito,
-            [ETypePayment.debit_card]: ETypePaymentFE.cartao_debito,
-            [ETypePayment.pix]: ETypePaymentFE.pix,
-            [ETypePayment.ticket]: ETypePaymentFE.boleto
-        };
-        return typePaymentMap[type];
+            
+        return allPaymentMethodActive
     }
 }

91c0176225e90f527564713a456969045dd74d38
Davi-pontes <davimartins2450@gmail.com>
2025-02-02 17:23:30 -0300

    feat: completed function to update add account

--- src/routes.ts
+++ src/routes.ts
@@ -8,17 +8,26 @@ import { LoginRoutes } from "./routes/login.routes";
 import { OrderRoutes } from "./routes/order.routes";
 import { PaymentMethodRoutes } from "./routes/paymentMethod.routes";
 import { UploadXlsx } from "./routes/upload-xlsx.routes";
-import { PublicUserRoutes } from "./routes/public/user.routes";
+//import { PublicUserRoutes } from "./routes/public/user.routes";
+import { CreateUserController } from "./controller/user/create-user";
 
 const routes = Router()
 
 routes.get("/",(req,res)=> {res.json({message: "hello"})})
+routes.post("/user/sign-up", async (req, res) => {
+    const createUserController = new CreateUserController()
+
+    const { body, statusCode } = await createUserController.handle({
+        body: req.body
+    })
+    res.status(statusCode).send(body)
+})
+//routes.use('/sign-up', PublicUserRoutes)
 routes.use('/product', ProductRoutes)
 routes.use('/product/ingredient', ProductIngredientRoutes)
 routes.use('/category', CategoryRoutes)
 routes.use('/stock', StockRoutes)
 routes.use('/user', UserRoutes)
-routes.use('/user/sign-up', PublicUserRoutes)
 routes.use('/login', LoginRoutes)
 routes.use('/order', OrderRoutes)
 routes.use('/payment/method', PaymentMethodRoutes)

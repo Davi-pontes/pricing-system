a1facb622ebdbd660b9f2acecb6c60501c6b00a1
Davi-pontes <davimartins2450@gmail.com>
2025-02-16 16:28:49 -0300

    feat: adjust

--- src/service/calculation/pricingProductWithIngredientCalculator.ts
+++ src/service/calculation/pricingProductWithIngredientCalculator.ts
@@ -1,11 +1,10 @@
-import { IPricingProductWithIngredientCalculator } from "@/interfaces/calculate";
 import { BaseCalculator } from "./baseCalculator";
 import { ICreateProductIngredientParams } from "@/interfaces/productIngredients";
 
 export class PricingProductWithIngredientCalculator
   extends BaseCalculator
 {
-  protected getTotalCostAllIngredients(
+  getTotalCostAllIngredients(
     data: Array<ICreateProductIngredientParams>
   ): number {
     return data.reduce(
@@ -15,30 +14,30 @@ export class PricingProductWithIngredientCalculator
     );
   }
 
-  protected getCostFixed(costAllIngredient: number, operationalCost: number) {
+  getCostFixed(costAllIngredient: number, operationalCost: number) {
     return (costAllIngredient * operationalCost) / 100;
   }
 
-  protected getPricePerUnit(cost: number, qtyInBox: number): number {
+  getPricePerUnit(cost: number, qtyInBox: number): number {
     return super.getPricePerUnit(cost, qtyInBox);
   }
 
-  protected getRevenueCost(
+  getRevenueCost(
     costAllIngredient: number,
     fixedCost: number,
   ): number {
     return costAllIngredient + fixedCost;
   }
 
-  protected getFinalRevenuePrice(profit: number, revenueCost: number): number {
+  getFinalRevenuePrice(profit: number, revenueCost: number): number {
     return profit + revenueCost
   }
 
-  protected getProfit(cost: number, profitPercentage: number): number {
+  getProfit(cost: number, profitPercentage: number): number {
     return super.getProfit(cost, profitPercentage);
   }
 
-  protected getProfitPercentage(cost: number, sellingPrice: number): number {
+  getProfitPercentage(cost: number, sellingPrice: number): number {
     return super.getProfitPercentage(cost, sellingPrice);
   }
 }
